35338eedfa0b72f0cd0a5ed84e0f111bc8c8dada
Lindsay Chapin <lnchapin@gmail.com>
2019-02-01 18:52:23 -0700

    finished

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,34 @@
-// add contact functionality
+$(document).ready(function(){
 
-// filter contact by type functionality
+  $("#addContact").on("submit", function(event){
+    event.preventDefault();
 
-// delete contact functionality
+    $.post("/api/contacts", {
+      firstName: $("#firstName").val().trim(),
+      lastName: $("#lastName").val().trim(),
+      contactType: $("#contactType").val(),
+      phoneNumber: $("#phoneNumber").val().trim(),
+      emailAddress: $("#emailAddress").val().trim()
+    })
+      .then(function(contact) {
+        window.location.replace("/");
+      });
+  });
+
+  $("#filterContacts").on("change",function(){
+    var type = $(this).val().toLowerCase();
+    window.location.replace(`/${type}`);
+  }
+  );
+
+  $(".delete").on("click", function(){
+    var id = $(this).parents("tr").data("id");
+    $.ajax({
+      method: "DELETE",
+      url: `/api/contacts/${id}`
+    }).then(function(contact) {
+      window.location.replace("/");
+    });
+  });
+
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,21 +3,43 @@ var db = require("../models");
 module.exports = function(app) {
     // this route should render the Handlebars 'form' template
 	app.get("/contacts/new", function(req, res) {
-        
+			res.render("form")
     });
 
-    // this route should find all contacts in the table and render them using the Handlebars 
+    // this route should find all contacts in the table and render them using the Handlebars
     // 'contacts' template, sorted ascending by firstName
     app.get("/", function(req, res) {
-
+			db.Contact.findAll({
+				order:[
+					['lastName', 'ASC'],
+					['firstName', 'ASC']
+				]
+			}).then(function(contacts){
+				res.render("contacts", {
+					name: "Lindsay",
+					contacts: contacts
+				})
+			})
     });
 
-    // this route should find all contacts of a particular type (Personal or Business) and render them 
+  // this route should find all contacts of a particular type (Personal or Business) and render them
     // using the Handlebars 'contacts' template, sorted ascending by firstName
     app.get("/:type", function(req, res) {
-
-        // BONUS: pass the contact type through to the handlebars template... be sure
-        // to make the contact type title-cased!
-
+			var type = req.params.type[0].toUpperCase() + req.params.type.slice(1);
+			db.Contact.findAll({
+				where: {
+					contactType: req.params.type
+				},
+				order:[
+					['lastName', 'ASC'],
+					['firstName', 'ASC']
+				]
+			}).then(function(contacts){
+				res.render("contacts", {
+					name: "Lindsay",
+					contacts: contacts,
+					type: type
+				})
+			})
     });
-}
+};
